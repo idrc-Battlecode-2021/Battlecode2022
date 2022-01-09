749883f86e70a83a3dda1a36bfa5d386723d6202
satviklolla <45611275+satviklolla@users.noreply.github.com>
2022-01-09 16:25:40 -0500

    update sense robot

--- src/bot3_MC/robots/buildings/Watchtower.java
+++ src/bot3_MC/robots/buildings/Watchtower.java
@@ -34,13 +34,28 @@ public class Watchtower extends Building {
         retransform();
         if(isDefensive){
             rc.setIndicatorString(isDefensive+" ");
-            MapLocation archonLoc = rc.senseRobot(myArchonID).getLocation();
-            if (rc.getLocation().isWithinDistanceSquared(archonLoc,2)){
+            if (rc.getLocation().isWithinDistanceSquared(archon,2)){
                 if (rc.getMode()!=RobotMode.PORTABLE){
-                    rc.transform();
+                    if (rc.canTransform()){
+                        rc.transform();
+                    }
                 }
                 else{
-                    intermediateMove(rc.getLocation().add(rc.getLocation().directionTo(archonLoc).opposite()));
+                   MapLocation away =  rc.getLocation().add(rc.getLocation().directionTo(archon).opposite());
+                    if (rc.onTheMap(away)){
+                        intermediateMove(away);
+                    }
+                    else{
+                        Direction direction = rc.getLocation().directionTo(archon);
+                        away = (archon.add(direction)).add(direction);
+                        if (rc.onTheMap(away)){
+                            intermediateMove(away);
+                        }
+                        else{
+                            if (rc.getMode()==RobotMode.PORTABLE && rc.canTransform()) rc.transform();
+                            attackDefensive();
+                        }
+                    }
                 }
             }
             else{
