03c22bb50172447a0a71bd7c5780b581486ef9dd
satviklolla <45611275+satviklolla@users.noreply.github.com>
2022-01-15 20:32:19 -0500

    Update Archon.java

--- src/bot6/robots/buildings/Archon.java
+++ src/bot6/robots/buildings/Archon.java
@@ -318,7 +318,7 @@ public class Archon extends Building{
         if (globalMinerCount < 6 || count%mod == 1){
             if (rc.getTeamLeadAmount(rc.getTeam())>=cost){
                 int i=0;
-                while (!rc.canBuildRobot(type,passableDirections.get(i)) && i<passableDirections.size()){
+                while (i<passableDirections.size() && !rc.canBuildRobot(type,passableDirections.get(i))){
                     i++;
                 }
                 if (rc.canBuildRobot(type,passableDirections.get(i))){
@@ -344,7 +344,7 @@ public class Archon extends Building{
             if (!checkBuildStatus(diff, cost)) return;
             if (rc.getTeamLeadAmount(rc.getTeam())>=cost){
                 int i=0;
-                while (!rc.canBuildRobot(type,passableDirections.get(i)) && i<passableDirections.size()){
+                while (i<passableDirections.size() && !rc.canBuildRobot(type,passableDirections.get(i)){
                     i++;
                 }
                 if (rc.canBuildRobot(type,passableDirections.get(i))){
