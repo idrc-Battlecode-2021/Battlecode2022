2718c8f568bbf7fe14fc967cc479a70892411486
Jeffrey Jiang <43833907+MechaDL7@users.noreply.github.com>
2022-01-17 11:16:43 -0500

    Update PathFindingSoldier.java

--- src/bot8/util/PathFindingSoldier.java
+++ src/bot8/util/PathFindingSoldier.java
@@ -1561,14 +1561,14 @@ public class PathFindingSoldier {
     //If too much bytecode, remove this method and directly put the switch statement into every if statement
     private int turnsToMove(MapLocation loc) throws GameActionException {
         switch(rc.senseRubble(loc)){
-            case  0: case  1: case  2: case  3:                            return 2;
-            case  4: case  5: case  6: case  7: case  8: case  9:          return 3;
-            case 10: case 11: case 12: case 13: case 14: case 15:          return 4;
-            case 16: case 17: case 18: case 19: case 20: case 21:          return 5;
-            case 22: case 23: case 24: case 25: case 26: case 27: case 28: return 6;
-            case 29: case 30: case 31: case 32: case 33: case 34:          return 7;
-            case 35: case 36: case 37: case 38: case 39: case 40:          return 8;
-            case 41: case 42: case 43: case 44: case 45: case 46:          return 9;
+            case  0: case  1: case  2: case  3:                            return  2;
+            case  4: case  5: case  6: case  7: case  8: case  9:          return  3;
+            case 10: case 11: case 12: case 13: case 14: case 15:          return  4;
+            case 16: case 17: case 18: case 19: case 20: case 21:          return  5;
+            case 22: case 23: case 24: case 25: case 26: case 27: case 28: return  6;
+            case 29: case 30: case 31: case 32: case 33: case 34:          return  7;
+            case 35: case 36: case 37: case 38: case 39: case 40:          return  8;
+            case 41: case 42: case 43: case 44: case 45: case 46:          return  9;
             case 47: case 48: case 49: case 50: case 51: case 52: case 53: return 10;
             case 54: case 55: case 56: case 57: case 58: case 59:          return 11;
             case 60: case 61: case 62: case 63: case 64: case 65:          return 12;
