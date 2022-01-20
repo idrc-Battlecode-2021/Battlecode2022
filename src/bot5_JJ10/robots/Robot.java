02b4b76b48101bfdaaa2930e3f1efe839d1bbc6d
Jeffrey Jiang <43833907+MechaDL7@users.noreply.github.com>
2022-01-20 12:10:23 -0500

    Update Robot.java

--- src/bot5_JJ10/robots/Robot.java
+++ src/bot5_JJ10/robots/Robot.java
@@ -447,7 +447,8 @@ public abstract class Robot {
     public MapLocation selectPriorityTarget() throws GameActionException {
         //returns location of target
         //returns own location if none
-        RobotInfo[] enemyRobots = rc.senseNearbyRobots(rc.getType().actionRadiusSquared, rc.getTeam().opponent());
+        //RobotInfo[] enemyRobotsSeen = rc.senseNearbyRobots(rc.getType().visionRadiusSquared,myTeam.opponent());
+        RobotInfo[] enemyRobots = rc.senseNearbyRobots(rc.getType().actionRadiusSquared, myTeam.opponent());
         RobotInfo[] myRobots = rc.senseNearbyRobots(rc.getType().visionRadiusSquared, rc.getTeam());
         RobotInfo archon=null, sage=null, lab=null, watchtower=null, soldier=null, miner=null, builder=null;
         int[] damages = {0,0,0,0,0}; //order corresponds with order of variables above
@@ -579,14 +580,14 @@ public abstract class Robot {
                 if (turns[minIndex]>25){
                     if (miner!=null){
                         target = miner.getLocation();
-                        moveToLowPassability();
+                        moveToLowPassability(target);
                         tryAttack(target);
                         rc.setIndicatorString("target: "+target);
                         return target;
                     }
                     else if (builder!=null){
                         target = builder.getLocation();
-                        moveToLowPassability();
+                        moveToLowPassability(target);
                         tryAttack(target);
                         rc.setIndicatorString("target: "+target);
                         return target;
@@ -623,8 +624,8 @@ public abstract class Robot {
         if (target==null){
             target=rc.getLocation();
         }
-        if (target!=rc.getLocation()){
-            moveToLowPassability();
+        if (!target.equals(rc.getLocation())){
+            moveToLowPassability(target);
             tryAttack(target);
         }
         rc.setIndicatorString("target: "+target);
@@ -632,7 +633,7 @@ public abstract class Robot {
         
     }
 
-    public boolean moveToLowPassability() throws GameActionException{
+    public boolean moveToLowPassability(MapLocation target) throws GameActionException{
         //TODO: doesn't guarantee that the target will be attacked
         //moves to lowest passability nearby
         if (!rc.isMovementReady()){
@@ -640,8 +641,33 @@ public abstract class Robot {
         }
         Direction lowest = Direction.CENTER;
         int lowest_rubble = rc.senseRubble(rc.getLocation());
+        if(rc.canSenseRobotAtLocation(target)){
+            RobotInfo robot = rc.senseRobotAtLocation(target);
+            if(rubbleActionTurnDiff(myLocation) < rubbleActionTurnDiff(target) || robot.getType() != RobotType.SOLDIER){
+                for (Direction d: Direction.allDirections()){
+                    MapLocation adjacent=rc.adjacentLocation(d);
+                    if(adjacent.distanceSquaredTo(target) > rc.getType().actionRadiusSquared)continue;
+                    if(rc.onTheMap(adjacent) && rc.canMove(d)){
+                        int rubbleAtLoc = rc.senseRubble(adjacent);
+                        if(rubbleAtLoc <lowest_rubble){
+                            lowest = d;
+                            lowest_rubble = rubbleAtLoc;
+                        }
+                    }
+                }
+                if (lowest==Direction.CENTER){
+                    return false;
+                }
+                if(rc.canMove(lowest)){
+                    rc.move(lowest);
+                    myLocation = rc.getLocation();
+                }
+                return true;
+            }
+        }
+
         for (Direction d: Direction.allDirections()){
-            MapLocation adjacent=rc.getLocation().add(d);
+            MapLocation adjacent=rc.adjacentLocation(d);
             if (rc.onTheMap(adjacent) && rc.canMove(d)){
                 int rubble = rc.senseRubble(adjacent);
                 if (rubble<lowest_rubble){
@@ -658,6 +684,26 @@ public abstract class Robot {
             myLocation = rc.getLocation();
         }
         return true;
+    }
+    private int rubbleActionTurnDiff(MapLocation loc) throws GameActionException {
+        int rubble = rc.senseRubble(loc);
+        switch (rc.getType()){
+            case SOLDIER:
+                switch (rubble){
+                    case 0: return 1;
+                    case 1: case 2: case 3: case 4: case 5: case 6: case 7: case 8: case 9: case 10: return 2;
+                    case 11: case 12: case 13: case 14: case 15: case 16: case 17: case 18: case 19: case 20: return 3;
+                    case 21: case 22: case 23: case 24: case 25: case 26: case 27: case 28: case 29: case 30: return 4;
+                    case 31: case 32: case 33: case 34: case 35: case 36: case 37: case 38: case 39: case 40: return 5;
+                    case 41: case 42: case 43: case 44: case 45: case 46: case 47: case 48: case 49: case 50: return 6;
+                    case 51: case 52: case 53: case 54: case 55: case 56: case 57: case 58: case 59: case 60: return 7;
+                    case 61: case 62: case 63: case 64: case 65: case 66: case 67: case 68: case 69: case 70: return 8;
+                    case 71: case 72: case 73: case 74: case 75: case 76: case 77: case 78: case 79: case 80: return 9;
+                    case 81: case 82: case 83: case 84: case 85: case 86: case 87: case 88: case 89: case 90: return 10;
+                    default: return 11;
+                }
+        }
+        return 0;
     }
     
     private void updateInternalMap(){}
